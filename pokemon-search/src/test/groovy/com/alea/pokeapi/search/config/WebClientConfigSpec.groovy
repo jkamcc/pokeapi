0b3e13c122517b3ba5a74c1316224cf2aead0ed9
jccarrillocasas <juancamilo.carrillocasas@altran.com>
2021-01-25 15:16:33 +0100

    FIX maven plugin management

--- pokemon-search/src/test/groovy/com/alea/pokeapi/search/config/WebClientConfigSpec.groovy
+++ pokemon-search/src/test/groovy/com/alea/pokeapi/search/config/WebClientConfigSpec.groovy
@@ -1,6 +1,5 @@
 package com.alea.pokeapi.search.config
 
-import WebClientConfig
 import spock.lang.Specification
 
 /**
@@ -12,9 +11,9 @@ class WebClientConfigSpec extends Specification {
         given: "web client config"
           def config = new WebClientConfig()
         when: "creating rest web client"
-            String apiUrl = "https://test.com/api/"
-            def webclient  = config.pokemonWebClient(apiUrl)
+          String apiUrl = "https://test.com/api/"
+          def webclient = config.pokemonWebClient(apiUrl)
         then: "poke api configuration is correct"
-            webclient != null
+          webclient != null
     }
 }
