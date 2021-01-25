b53d4db8f153717450b2e61ffbe1b6a21cb5494c
jccarrillocasas <juancamilo.carrillocasas@altran.com>
2021-01-25 12:10:00 +0100

    REFACTOR rename spring boot app

--- src/main/java/com/alea/pokeapi/PokeApiApplication.java
+++ src/main/java/com/alea/pokeapi/PokeApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PokeapiApplication {
+public class PokeApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PokeapiApplication.class, args);
+        SpringApplication.run(PokeApiApplication.class, args);
     }
 
 }
--- src/test/java/com/alea/pokeapi/PokeApiApplicationTests.java
+++ src/test/java/com/alea/pokeapi/PokeApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PokeapiApplicationTests {
+class PokeApiApplicationTests {
 
     @Test
     void contextLoads() {
