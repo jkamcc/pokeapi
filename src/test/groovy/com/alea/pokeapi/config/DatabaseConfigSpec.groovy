9088edbc7bb09d83fa048357106e1aaff4ee75bd
jccarrillocasas <juancamilo.carrillocasas@altran.com>
2021-01-25 12:53:47 +0100

    ADD database configuration test

---
+++ src/test/groovy/com/alea/pokeapi/config/DatabaseConfigSpec.groovy
@@ -0,0 +1,22 @@
+package com.alea.pokeapi.config
+
+import io.r2dbc.spi.ConnectionFactory
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
+import spock.lang.Specification
+
+/**
+ * @author altran
+ */
+class DatabaseConfigSpec extends Specification {
+
+    def "check database configuration is loaded with database populator"() {
+        given: "configuration"
+          def config = new DatabaseConfig()
+        when: "initialized"
+          ConnectionFactoryInitializer initializer = config.initializer(Mock(ConnectionFactory.class))
+        then: "memory db loaded config enabled"
+          notThrown(Exception)
+          initializer != null
+          initializer.databasePopulator != null
+    }
+}
